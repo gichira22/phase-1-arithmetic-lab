fd6ec703b888e8ad5b36c746c204d41efcb27eb0
Virginia Gichira <gichiravirginia@gmail.com>
2024-03-26 20:50:05 +0300

    add

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+// An equation whose multiplied results will equal 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+// Generate a random integer greater than 0
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random); 
+
+// calculate the remainder of 2 variables to generate remainder 4
+let num3 = 79;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+console.log(mod); 
+
+// Outputs the largest number as 20 from a sequence
+
+let numbers = (15,10,7,18,20);
+let max = Math.max(numbers); 
+
+console.log(max);
